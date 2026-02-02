03d51f82eef88e1b2a5a93f5777f32c2e4ac1032
Bankbeng <banksinghalath1@gmail.com>
2026-02-02 18:13:52 +0700

    update

--- app/routes/product.route.js
+++ app/routes/product.route.js
@@ -3,8 +3,10 @@ module.exports = app => {
 
   const products = require("../controllers/product.controller");
 
-  app.get("/products", auth, products.findAll);
-  app.post("/products", auth, products.create);
+  //app.get("/products", auth, products.findAll);
+  app.get("/products", products.findAll);
+  //app.post("/products", auth, products.create);
+  app.post("/products", products.create);
   app.get("/products/:id", auth, products.findOne);
   app.put("/products/:id", auth, products.update);
   app.delete("/products/:id", auth, products.delete);
--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const app = express();
 
 const cors = require("cors");
 
-app.use(cors({
-  origin: "http://localhost:3001",
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
